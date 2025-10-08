7bede25eb6835460ff8b6c2c56c02321fdec4322
Alexander Lototsky <alexander.lototsky@gmail.com>
2025-10-08 23:45:19 +0300

    Add svelte

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 // vite.config.js
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
     base: '/devtools/',
-});
+    plugins: [svelte()]
+})
